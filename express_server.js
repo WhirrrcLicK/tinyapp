d96d0fa49e56cfb133372d129e9550d5418d8137
Kaiya Hedley <keshedley@gmail.com>
2022-08-09 18:46:07 -0600

    tiny url generated and redirected to id page

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.set("view engine", "ejs")
 
@@ -11,34 +13,48 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {}
-app.use(express.urlencoded({extended: true}))
+function generateRandomString() {
+  let randomString = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return randomString;
+}
+
+//ROUTES
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
+});
+
+app.get("/urls/new", (req, res) => {
+  // console.log("urls_new");
+  //request: asking for form response: getting it back
+  res.render("urls_new");
+});
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars)
-})
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  let longURL = req.body.longURL
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = longURL
+  console.log(urlDatabase)
+  res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars)
-})
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
